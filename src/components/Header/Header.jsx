f4c97362def72f71c9f724048942133fa78806df
Marwa Mohamed <marwa.m7d@gmail.com>
2024-12-21 23:48:27 -0500

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
+      <h1>My React App</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// filepath: /tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
