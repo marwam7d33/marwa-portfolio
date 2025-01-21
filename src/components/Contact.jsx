43fe702eb637ac40de504a1c64e2f6279171c863
Marwa Mohamed <marwa.m7d@gmail.com>
2025-01-21 17:14:25 -0500

    contact form - activate

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 const socialLinks = [
   {
-    href: "https://www.github.com/codewithsadee-org",
+    href: "https://github.com/marwam7d33",
     icon: (
       <svg
         width="24"
@@ -18,7 +18,7 @@ const socialLinks = [
     alt: "GitHub",
   },
   {
-    href: "https://www.linkedin.com/in/codewithsadee",
+    href: "https://www.linkedin.com/in/marwa-mohamed01/",
     icon: (
       <svg
         width="24"
@@ -40,25 +40,37 @@ const socialLinks = [
 const Contact = () => {
   return (
     <section id="contact" className="section">
-      <div className="container">
-        <div className="">
-          <h2 className="headline-2">Contact me for collaboration</h2>
-          <p className="">
+      <div className="container lg:grid lg:grid-cols-2 lg:items-stretch">
+        <div className="mb-12 lg:mb-0 lg:flex lg:flex-col">
+          <h2 className="headline-2 lg:max-w-[12ch]">
+            Contact me for collaboration
+          </h2>
+          <p className="text-zinc-400 mt-3 mb-8 max-w-[50ch] lg:max-w-[30ch] ">
             Get in touch today to explore your project needs and begin creating
             something extraordinary together!
           </p>
-          <div className="">
+          <div className="flex items-center gap-2 mt-auto">
             {socialLinks.map(({ href, icon }, key) => (
-              <a key={key} href={href} target="_blank" className="">
+              <a
+                key={key}
+                href={href}
+                target="_blank"
+                className="w-12 h-12 grid place-items-center ring-inset ring-2 ring-zinc-50/5 rounded-lg transition-[background-color,color]
+               hover:bg-zinc-50 active:bg-zinc-50/80"
+              >
                 {icon}
               </a>
             ))}
           </div>
         </div>
 
-        <form action="" method="POST" className="">
-          <div className="">
-            <div className="">
+        <form
+          action="https://getform.io/f/azyymlgb"
+          method="POST"
+          className="xl:pl-10 2xl:pl-20"
+        >
+          <div className="md:grid md:items-center md:grid-cols-2 md:gap-2">
+            <div className="mb-4">
               <label htmlFor="name" className="label">
                 Name
               </label>
@@ -72,7 +84,7 @@ const Contact = () => {
                 className="text-field"
               />
             </div>
-            <div className="">
+            <div className="mb-4">
               <label htmlFor="email" className="label">
                 Email
               </label>
@@ -94,12 +106,15 @@ const Contact = () => {
             <textarea
               name="message"
               id="message"
-              placeholder="Hi!"
+              placeholder="type here..."
               required
-              className="text-field"
+              className="text-field resize-y min-h-32 max-h-80"
             ></textarea>
           </div>
-          <button type="submit" className="btn btn-primary">
+          <button
+            type="submit"
+            className="btn btn-primary [&]:max-w-full w-full justify-center"
+          >
             Submit{" "}
           </button>
         </form>
